f9827e99daef75866ef05af234326364925729ee
Eric Mouledoux <EricMouledoux@gmail.com>
2016-08-02 09:46:53 -0500

    Added scripts from original project

---
+++ Assets/Scripts/CameraToMouseTracking.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraToMouseTracking : MonoBehaviour
+{
+    void Awake()
+    {
+        MainCamera = Camera.main;
+    }
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine("MouseToCamera");
+        }
+	}
+
+    IEnumerator MouseToCamera()
+    {
+        Vector3 initCursorPos = Input.mousePosition;
+        Vector3 initCameraRot = MainCamera.transform.localEulerAngles;
+
+        while(Input.GetMouseButton(0))
+        {
+            ///Relative Cursor Position
+            Vector3 rcp = initCursorPos - Input.mousePosition;
+            /// New Camera Position
+            Vector3 ncp = new Vector3(-rcp.y, rcp.x, 0);
+
+            MainCamera.transform.localEulerAngles += ncp * (CameraSpeed * Time.deltaTime);
+
+            ncp = new Vector3(
+                MainCamera.transform.localEulerAngles.x,
+                MainCamera.transform.localEulerAngles.y,
+                0);
+
+            MainCamera.transform.localEulerAngles = ncp;
+
+            initCursorPos = Input.mousePosition;
+            yield return null;
+        }
+    }
+
+    public float CameraSpeed = 1f;
+
+    private Camera MainCamera;
+    private Vector3 m_mousePos;
+}
---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VideoManager : MonoBehaviour
+{
+    void Awake()
+    {
+        for (int i = 0; i < VideoUrls.Count; i++)
+        {
+            MediaPlayerCtrl pl = gameObject.AddComponent<MediaPlayerCtrl>();
+            GameObject screen = Instantiate(Resources.Load("SphereScreen")) as GameObject;
+            screen.SetActive(false);
+
+            pl.m_bInit = false;
+            pl.m_bAutoPlay = false;
+            pl.m_bLoop = false;
+
+            pl.m_TargetMaterial = new GameObject[1];
+            pl.m_TargetMaterial[0] = screen;
+
+            pl.m_strFileName = VideoUrls[i];
+
+            pl.m_TargetMaterial[0].name = pl.m_strFileName;
+
+            preLoaded.Add(pl);
+        }
+
+        preLoaded[currentVideoIndex].m_TargetMaterial[0].SetActive(true);
+
+        StartCoroutine(Preload());
+    }
+
+    public void PlayVideoAtIndex(int index)
+    {
+        if (currentVideoIndex == index)
+        {
+            preLoaded[currentVideoIndex].Play();
+            return;
+        }
+
+        preLoaded[currentVideoIndex].Stop();
+        preLoaded[currentVideoIndex].m_TargetMaterial[0].SetActive(false);
+
+        currentVideoIndex = index;
+
+        preLoaded[currentVideoIndex].Play();
+        preLoaded[currentVideoIndex].m_TargetMaterial[0].SetActive(true);
+    }
+
+    public void PlayNextVideo()
+    {
+        PlayVideoAtIndex(currentVideoIndex + 1);
+    }
+
+    public void TogglePlayPause()
+    {
+        MediaPlayerCtrl.MEDIAPLAYER_STATE cState = preLoaded[currentVideoIndex].GetCurrentState();
+
+        if (cState == MediaPlayerCtrl.MEDIAPLAYER_STATE.PLAYING)
+        {
+            preLoaded[currentVideoIndex].Pause();
+        }
+
+        else if (cState == MediaPlayerCtrl.MEDIAPLAYER_STATE.READY ||
+                 cState == MediaPlayerCtrl.MEDIAPLAYER_STATE.PAUSED ||
+                 cState == MediaPlayerCtrl.MEDIAPLAYER_STATE.STOPPED ||
+                 cState == MediaPlayerCtrl.MEDIAPLAYER_STATE.END)
+        {
+            preLoaded[currentVideoIndex].Play();
+        }
+    }
+
+    IEnumerator Preload(int index = 0)
+    {
+        preLoaded[index].Load(preLoaded[index].m_strFileName);
+
+        while (preLoaded[index].GetCurrentSeekPercent() < 99)
+        {
+            t.text = index.ToString() + " @ " + preLoaded[index].GetCurrentSeekPercent().ToString(); 
+            yield return null;
+        }
+
+        preLoaded[index].Play();
+
+        while (preLoaded[index].GetCurrentState() != MediaPlayerCtrl.MEDIAPLAYER_STATE.PLAYING)
+        {
+            yield return null;
+        }
+
+        preLoaded[index].Pause();
+
+        preLoaded[index].OnEnd += PlayNextVideo;
+
+        t.text = index.ToString() + " Done";
+
+        if (index + 1 < preLoaded.Count)
+            StartCoroutine(Preload(++index));
+        
+    }
+
+    public Text t;
+    public List<string> VideoUrls = new List<string>();
+
+    [SerializeField]
+    private int currentVideoIndex = 0;
+    private List<MediaPlayerCtrl> preLoaded = new List<MediaPlayerCtrl>();
+}
