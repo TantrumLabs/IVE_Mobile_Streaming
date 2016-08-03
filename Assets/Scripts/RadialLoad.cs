23983a1bb83be2e0be9bdf13e19816e78ee21bf1
Zac King <ZacKingx@gmail.com>
2016-08-03 18:11:02 -0500

    Fix Raycast Load
    need to clean up the code and take out the bits that don't work now

--- Assets/Scripts/RadialLoad.cs
+++ Assets/Scripts/RadialLoad.cs
@@ -2,47 +2,61 @@
 using UnityEngine.UI;
 using System.Collections;
 
-[RequireComponent (typeof(Image))]
 public class RadialLoad : MonoBehaviour
 {
     #region Variables
-    private Image progressCircle;                   // 
+    [SerializeField] private Image progressCircle;                   // 
     [SerializeField] private float loadTime = 3f;   // Seconds to complete load
     [SerializeField] private Gradient loadGradient; // Gradient over time for load
     public bool isLoading = false;                  // Bool for current state of loading 
+    VideoManager vm;
+
     #endregion
 
     // MonoBehaviour ///////////////////////////////////////////////////////////////////////////////////
     [ContextMenu("Set the Type to Fill")]
     void Awake()    // If not already set up, it will be fixed on awake
     {
-        progressCircle        = gameObject.GetComponent<Image>();
+        vm = FindObjectOfType<VideoManager>();
         progressCircle.type   = Image.Type.Filled;
     }
     
     // Context Menu Functions //////////////////////////////////////////////////////////////////////////
-    [ContextMenu("Test Load")]
-    private void testLoad()                 // Simulate an outside function influence on script
-    {
-        Callback c = assdf;
-        isLoading = true;
-        LoadTarget(3, c);
-    }
+    //[ContextMenu("Test Load")]
+    //private void testLoad() // Simulate an outside function influence on script
+    //{
+    //    Callback c = assdf;
+    //    isLoading = true;
+    //    LoadTarget(3, c);
+    //}
 
-    void assdf()
-    {
-        Debug.Log("Loading complete");
-    }
-    
+    //void assdf()
+    //{
+    //    Debug.Log("Loading complete");
+    //}
+
     // Functions ///////////////////////////////////////////////////////////////////////////////////////
-    public void LoadTarget(float loadTime, Callback a)
+    public void StopLoad()  //
     {
-        loadTime = loadTime;
-        StartCoroutine(LoadCircle(a));
+        isLoading = false;
     }
 
-    IEnumerator LoadCircle(Callback callback)    // Load coroutine
+    public void LoadTarget(float time, int vidIndex)  //
     {
+        isLoading = true;
+        loadTime = time;
+        StartCoroutine(LoadCircle(vidIndex));
+    }
+    //public void LoadTarget<T>(float time, Callback<T> a, T arg1)  //
+    //{
+    //    loadTime = time;
+    //    StartCoroutine(LoadCircle(a, arg1));
+    //}
+
+
+    IEnumerator LoadCircle(int index)    // Load coroutine
+    {
+        Debug.Log("Load started");
         float timer = 0;
         while (timer <= loadTime && isLoading)
         {
@@ -54,9 +68,30 @@ public class RadialLoad : MonoBehaviour
 
         if(isLoading)
         {
-            callback();
+            //vm.PlayVideoAt(index);
+            Debug.Log("Load done");
         }
 
         progressCircle.fillAmount = 0;   // Reset
     }
+    //IEnumerator LoadCircle<T>(Callback<T> callback, T arg1)    // Load coroutine
+    //{
+    //    float timer = 0;
+    //    while (timer <= loadTime && isLoading)
+    //    {
+    //        progressCircle.fillAmount = timer / loadTime;
+    //        progressCircle.color = loadGradient.Evaluate(timer / loadTime);
+    //        timer += Time.deltaTime;
+    //        yield return null;
+    //    }
+
+    //    if (isLoading)
+    //    {
+    //        callback(arg1);
+    //    }
+
+    //    progressCircle.fillAmount = 0;   // Reset
+    //}
+
+
 }
--- Assets/Scripts/RaySelect.cs
+++ Assets/Scripts/RaySelect.cs
@@ -6,21 +6,40 @@ public class RaySelect : MonoBehaviour
 {
     private RaycastHit rayCursor;   //
     private RadialLoad loader;      //
-
+    int targetIndex;                //
+    //VideoManager vm;                //
     // MonoBehaviour ///////////////////////////////////////////////////////////////////////////////////
-    void Update ()
+    void Start()
     {
-        Physics.Raycast(transform.position, transform.forward, out rayCursor, 100);
+        //vm = FindObjectOfType<VideoManager>();
+        loader = gameObject.GetComponent<RadialLoad>();
+    }
+
+    void FixedUpdate ()
+    {
+        //target = rayCursor.collider.gameObject.GetComponent<Clickable>();
+        if (Physics.Raycast(transform.position, transform.forward, out rayCursor, 100))
+        {
+            if(!loader.isLoading)
+                OnSelectionHit();   // RayCast on a valid Selection Volume
+        }
+        else
+        {
+            OnSelectionExit();  // RayCast is no longer on a Selection Volume
+        }
 
     }
 
-    void OnSelecetionHit()
+    // Functions ///////////////////////////////////////////////////////////////////////////////////////
+    void OnSelectionHit()   // Start Load
     {
-        // call loader function
+        targetIndex = rayCursor.collider.gameObject.GetComponent<Clickable>().VideoIndex;
+        //Callback<int> cb = new Callback<int>(vm.PlayVideoAt);
+        loader.LoadTarget(2, targetIndex);
     }
 
-    void OnSelectionExit()
+    void OnSelectionExit()  // Stop Load
     {
-        loader.isLoading = false;
+        loader.StopLoad();
     }
 }
